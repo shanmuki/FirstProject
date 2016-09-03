cdc719600c305e8f5358b146699c7d897bfb73fb
shanmuki <shanmukiraghavan@gmail.com>
2016-09-02 19:54:32 -0700

    Sample ClassExample

---
+++ ClassExample.java
@@ -0,0 +1,80 @@
+/**
+ * 
+ */
+package com.apex.thirtydaysofcode;
+import static java.lang.System.out;
+
+/**
+ * @author Shanmuki
+ *
+ */
+
+
+public class Person
+{
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	/**
+	 * @param age
+	 */
+	public Person(int age) {
+		super();
+		
+		if (age < 0) 
+		{
+			this.age = 0;
+			
+		}
+		else 
+		{
+			this.age = age;
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+	private int age; 
+	
+	
+}
+
+
+public class ClassExample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		try
+		{
+			
+			
+			
+			
+		}
+		catch(Exception e)
+		{
+			
+		}
+	
+	
+	}
+	
+
+}
+
+
